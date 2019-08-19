3e608a8f3833f08fe0330e5052a25ce2e26b3fad
silvajohnny777 <jonatha_silva2012@hotmail.com>
2019-08-19 12:10:44 -0300

    Updates

---
+++ precache-manifest.78dcf875330848ba062636a35f825b7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0a92c2229cd533ed7e580035509575",
+    "url": "/ReactApiCountries/index.html"
+  },
+  {
+    "revision": "c516c654dcefce37d538",
+    "url": "/ReactApiCountries/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "72d32023a5fba8ed5444",
+    "url": "/ReactApiCountries/static/js/2.9fcca981.chunk.js"
+  },
+  {
+    "revision": "c516c654dcefce37d538",
+    "url": "/ReactApiCountries/static/js/main.59d30579.chunk.js"
+  },
+  {
+    "revision": "3b87cf7098083d042be6",
+    "url": "/ReactApiCountries/static/js/runtime~main.774dc77a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactApiCountries/static/media/logo.5d5d9eef.svg"
+  }
+]);
